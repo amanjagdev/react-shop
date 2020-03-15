ff71975eb6399aaf941faecc3ba9561b60cdbb90
amanjagdev <amankumarjagdev@gmail.com>
2020-03-15 16:31:31 +0530

    Made Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Navigation.css';
+import {Navbar, Nav , NavDropdown , Form , FormControl, Button} from 'react-bootstrap'
 
 const Navigation = props => {
     return(
     <>
-        <h1>Navigation</h1>
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React Shop</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/cart">Cart</Nav.Link>
+                </Nav>
+                {/* <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-success">Search</Button>
+                </Form> */}
+            </Navbar.Collapse>
+        </Navbar>
     </>);
 }
 
