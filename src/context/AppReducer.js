0257c43185b27dab3573d7de704466b7954f713e
amanjagdev <amankumarjagdev@gmail.com>
2020-03-23 13:25:48 +0530

    Quantity management resolved

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,11 +1,17 @@
 export default (state, action) => {
     switch(action.type) {
-        case 'ADD_CART' : 
-            // state.cart.forEach((item) => {
-            //     if(item.id === action.payload.id){
-            //       // Return a array with quantity increased
-            //     }
-            // });
+        case 'ADD_CART' :
+            console.log(action.payload.id);
+            const temp = state.cart.filter(item => item.id === action.payload.id)
+            const restArray = state.cart.filter(item => item.id !== action.payload.id)
+            console.log(temp);
+            if(temp[0] != null){
+                temp[0].qty++;
+                return {
+                    ...state,
+                    cart: [temp[0], ...restArray]
+                }
+            }
             return {
                 ...state,
                 cart: [action.payload, ...state.cart]
