cb797166ad1630f87c3a38ddca67b4b7b3da17f2
amanjagdev <amankumarjagdev@gmail.com>
2020-03-23 13:02:58 +0530

    Add product now working and finalised

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,43 +1,61 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import './AddProduct.css';
 import {GlobalContext} from '../../context/GlobalState';
-import {Form, Button} from 'react-bootstrap'
+// import {Form, Button} from 'react-bootstrap'
 
 const AddProduct = props => {
 
-  const addProduct = useContext(GlobalContext);
+  const {addProduct} = useContext(GlobalContext);
 
-  const handleSubmit = (data) => {
-    console.log("Added Product");
-    addProduct(data);
+  //use state hooks to manage data of product
+  const [rowno,
+    setRowno] = useState(21);
+  const [desc,
+    setDesc] = useState('');
+  const [name,
+    setName] = useState('');
+  const [price,
+    setPrice] = useState(0);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newItem = {
+      id: rowno,
+      name,
+      desc,
+      price: Number(price),
+      qty: 0
+    }
+    setRowno(rowno + 1);
+    console.log(newItem);
+
+    addProduct(newItem);
   }
 
   return (
     <React.Fragment >
       <div className="container">
         <h1>Add Product</h1>
-        <Form onSubmit={handleSubmit}>
 
-          <Form.Group controlId="name">
-            <Form.Label>Product Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter Name of product"/>
-          </Form.Group>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Product Name</label>
+            <input type="text" className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)}></input>
+          </div>
+          <div className="form-group">
+            <label htmlFor="desc">Product Description</label>
+            <input type="text" className="form-control" id="desc" value={desc} onChange={(e) => setDesc(e.target.value)}></input>
+          </div>
+          <div className="form-group">
+            <label htmlFor="price">Product Price</label>
+            <input type="number" className="form-control" id="price" value={price} onChange={(e) => setPrice(e.target.value)}></input>
+          </div>
 
-          <Form.Group controlId="desc">
-            <Form.Label>Product Inforamtion</Form.Label>
-            <Form.Control type="text" placeholder="Information about product"/>
-          </Form.Group>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
 
-          <Form.Group controlId="price">
-            <Form.Label>Product Price</Form.Label>
-            <Form.Control type="number" placeholder="Price of Product"/>
-          </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-          
-        </Form>
       </div>
     </React.Fragment>
   );
